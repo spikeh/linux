550c9ed9ecee4b9f2746cd79e2dc9bedf8821bbe
David Wei <davidhwei@meta.com>
2023-08-10 15:49:37 -0700

    io_uring: setup ZC for an Rx queue when registering an ifq
    RFC only, not for upstream
    
    Just as with the previous patch, it will be migrated from ndo_bpf
    
    This patch sets up ZC for an Rx queue in a net device when an ifq is
    registered with io_uring. The Rx queue is specified in the registration
    struct.
    
    For now since there is only one ifq, its destruction is implicit during
    io_uring cleanup.
    
    Signed-off-by: David Wei <dw@davidwei.uk>

--- io_uring/zc_rx.c
+++ io_uring/zc_rx.c
@@ -33,6 +33,34 @@ struct io_zc_refill_data {
 	struct io_zc_rx_buf *buf;
 };
 
+typedef int (*bpf_op_t)(struct net_device *dev, struct netdev_bpf *bpf);
+
+static int __io_queue_mgmt(struct net_device *dev, struct io_zc_rx_ifq *ifq,
+			   u16 queue_id)
+{
+	struct netdev_bpf cmd;
+	bpf_op_t ndo_bpf;
+
+	ndo_bpf = dev->netdev_ops->ndo_bpf;
+	if (!ndo_bpf)
+		return -EINVAL;
+
+	cmd.command = XDP_SETUP_ZC_RX;
+	cmd.zc_rx.ifq = ifq;
+	cmd.zc_rx.queue_id = queue_id;
+	return ndo_bpf(dev, &cmd);
+}
+
+static int io_open_zc_rxq(struct io_zc_rx_ifq *ifq)
+{
+	return __io_queue_mgmt(ifq->dev, ifq, ifq->if_rxq_id);
+}
+
+static int io_close_zc_rxq(struct io_zc_rx_ifq *ifq)
+{
+	return __io_queue_mgmt(ifq->dev, NULL, ifq->if_rxq_id);
+}
+
 static int io_allocate_rbuf_ring(struct io_zc_rx_ifq *ifq,
 				 struct io_uring_zc_rx_ifq_reg *reg)
 {
@@ -189,6 +217,11 @@ static void io_shutdown_ifq(struct io_zc_rx_ifq *ifq)
 		}
 	}
 	ifq->nr_sockets = 0;
+
+	if (ifq->if_rxq_id != -1) {
+		io_close_zc_rxq(ifq);
+		ifq->if_rxq_id = -1;
+	}
 }
 
 static void io_zc_rx_ifq_free(struct io_zc_rx_ifq *ifq)
@@ -196,6 +229,10 @@ static void io_zc_rx_ifq_free(struct io_zc_rx_ifq *ifq)
 	io_shutdown_ifq(ifq);
 	if (ifq->pool)
 		io_zc_rx_free_pool(ifq->pool);
+	if (ifq->if_rxq_id != -1)
+		io_close_zc_rxq(ifq);
+	if (ifq->dev)
+		dev_put(ifq->dev);
 	io_free_rbuf_ring(ifq);
 	kfree(ifq);
 }
@@ -226,12 +263,21 @@ int io_register_zc_rx_ifq(struct io_ring_ctx *ctx,
 	if (ret)
 		goto err;
 
+	ret = -ENODEV;
+	ifq->dev = dev_get_by_index(current->nsproxy->net_ns, reg.if_idx);
+	if (!ifq->dev)
+		goto err;
+
 	ret = io_zc_rx_create_pool(ctx, ifq, reg.region_id);
 	if (ret)
 		goto err;
 
 	ifq->rq_entries = reg.rq_entries;
 	ifq->if_rxq_id = reg.if_rxq_id;
+
+	ret = io_open_zc_rxq(ifq);
+	if (ret)
+		goto err;
 
 	ring_sz = sizeof(struct io_uring);
 	rqes_sz = sizeof(struct io_uring_rbuf_rqe) * ifq->rq_entries;
@@ -241,6 +287,7 @@ int io_register_zc_rx_ifq(struct io_ring_ctx *ctx,
 	reg.rq_off.tail = offsetof(struct io_uring, tail);
 
 	if (copy_to_user(arg, &reg, sizeof(reg))) {
+		io_close_zc_rxq(ifq);
 		ret = -EFAULT;
 		goto err;
 	}
