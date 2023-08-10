7556ac6c41b10ba330f813203ae9a082b4206742
David Wei <davidhwei@meta.com>
2023-08-10 15:27:06 -0700

    netdev: add XDP_SETUP_ZC_RX command
    RFC only, not for upstream.
    This will be replaced with a separate ndo callback or some other
    mechanism in next patchset revisions.
    
    This patch adds a new XDP_SETUP_ZC_RX command that will be used in a
    later patch to enable or disable ZC RX for a specific RX queue.
    
    We are open to suggestions on a better way of doing this. Google's TCP
    devmem proposal sets up struct netdev_rx_queue which persists across
    device reset, then expects userspace to use an out-of-band method (e.g.
    ethtool) to reset the device, thus re-filling a hardware Rx queue.
    
    Signed-off-by: David Wei <dw@davidwei.uk>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1009,6 +1009,7 @@ enum bpf_netdev_command {
 	BPF_OFFLOAD_MAP_ALLOC,
 	BPF_OFFLOAD_MAP_FREE,
 	XDP_SETUP_XSK_POOL,
+	XDP_SETUP_ZC_RX,
 };
 
 struct bpf_prog_offload_ops;
@@ -1047,6 +1048,11 @@ struct netdev_bpf {
 			struct xsk_buff_pool *pool;
 			u16 queue_id;
 		} xsk;
+		/* XDP_SETUP_ZC_RX */
+		struct {
+			struct io_zc_rx_ifq *ifq;
+			u16 queue_id;
+		} zc_rx;
 	};
 };
 
