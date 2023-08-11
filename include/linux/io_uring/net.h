bdf6138a34a2f97c1bb3d2515604fd55f82df3fb
David Wei <davidhwei@meta.com>
2023-08-11 16:16:04 -0700

    io_uring: add zero copy buf representation and pool
    This patch adds two objects:
    
    * Zero copy buffer representation, holding a page and a net_iov. The
      page is needed as net_iov is designed for opaque device memory,
      whereas we are backed by real pages.
    * Zero copy pool, spiritually similar to page pool, that holds ZC bufs
      and hands them out to net devices. This will be used as an
      implementation of page pool memory provider.
    
    Pool regions are registered w/ io_uring using the registered buffer API,
    with a 1:1 mapping between region and nr_iovec in
    io_uring_register_buffers. This does the heavy lifting of pinning and
    chunking into bvecs into a struct io_mapped_ubuf for us.
    
    For now as there is only one pool region per ifq, there is no separate
    API for adding/removing regions yet and it is mapped implicitly during
    ifq registration.
    
    Signed-off-by: David Wei <dw@davidwei.uk>

--- include/linux/io_uring/net.h
+++ include/linux/io_uring/net.h
@@ -2,7 +2,14 @@
 #ifndef _LINUX_IO_URING_NET_H
 #define _LINUX_IO_URING_NET_H
 
+#include <net/page_pool/types.h>
+
 struct io_uring_cmd;
+
+struct io_zc_rx_buf {
+	struct net_iov		niov;
+	struct page		*page;
+};
 
 #if defined(CONFIG_IO_URING)
 int io_uring_cmd_sock(struct io_uring_cmd *cmd, unsigned int issue_flags);
--- io_uring/zc_rx.h
+++ io_uring/zc_rx.h
@@ -3,15 +3,30 @@
 #define IOU_ZC_RX_H
 
 #include <linux/io_uring_types.h>
+#include <linux/io_uring/net.h>
 #include <linux/skbuff.h>
 
 #define IO_ZC_MAX_IFQ_SOCKETS		16
 #define IO_ZC_IFQ_IDX_OFFSET		16
 #define IO_ZC_IFQ_IDX_MASK		((1U << IO_ZC_IFQ_IDX_OFFSET) - 1)
 
+struct io_zc_rx_pool {
+	struct io_zc_rx_ifq	*ifq;
+	struct io_zc_rx_buf	*bufs;
+	u32			nr_bufs;
+	u16			pool_id;
+
+	/* freelist */
+	spinlock_t		freelist_lock;
+	u32			free_count;
+	u32			freelist[];
+};
+
 struct io_zc_rx_ifq {
 	struct io_ring_ctx		*ctx;
 	struct net_device		*dev;
+	struct io_zc_rx_pool		*pool;
+
 	struct io_uring			*rq_ring;
 	struct io_uring_rbuf_rqe 	*rqes;
 	u32				rq_entries;
