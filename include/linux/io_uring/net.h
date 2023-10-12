e1f8d572af094be12bc7ede40087b43ac8a82e0c
Pavel Begunkov <asml.silence@gmail.com>
2023-10-12 12:10:37 +0100

    [NOT-FOR-UPSTREAM] veth: add support for io_uring zc rx
    BUGGY!
    
    Add io_uring zerocopy support for veth. It's not truly zerocopy, the
    data is copied in napi, but that's early in the stack and so useful
    for now for testing.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- include/linux/io_uring/net.h
+++ include/linux/io_uring/net.h
@@ -2,9 +2,18 @@
 #ifndef _LINUX_IO_URING_NET_H
 #define _LINUX_IO_URING_NET_H
 
+#include <net/page_pool/types.h>
+
 struct io_uring_cmd;
 
+/* not for upstream, do it the dirty way */
+
 #if defined(CONFIG_IO_URING)
+
+#if defined(CONFIG_PAGE_POOL)
+struct page *io_iov_get_page(netmem_ref netmem);
+#endif
+
 int io_uring_cmd_sock(struct io_uring_cmd *cmd, unsigned int issue_flags);
 
 #else
