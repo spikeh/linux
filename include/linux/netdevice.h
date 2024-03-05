fa096cd64be38c7a6e489b52788bddd516f4358c
Mina Almasry <almasrymina@google.com>
2024-03-04 18:01:36 -0800

    queue_api: define queue api
    This API enables the net stack to reset the queues used for devmem.
    
    Signed-off-by: Mina Almasry <almasrymina@google.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1351,6 +1351,20 @@ struct netdev_net_notifier {
  *			   struct kernel_hwtstamp_config *kernel_config,
  *			   struct netlink_ext_ack *extack);
  *	Change the hardware timestamping parameters for NIC device.
+ *
+ * void *(*ndo_queue_mem_alloc)(struct net_device *dev, int idx);
+ *	Allocate memory for an RX queue. The memory returned in the form of
+ *	a void * can be passed to ndo_queue_mem_free() for freeing or to
+ *	ndo_queue_start to create an RX queue with this memory.
+ *
+ * void	(*ndo_queue_mem_free)(struct net_device *dev, void *);
+ *	Free memory from an RX queue.
+ *
+ * int (*ndo_queue_start)(struct net_device *dev, int idx, void *);
+ *	Start an RX queue at the specified index.
+ *
+ * int (*ndo_queue_stop)(struct net_device *dev, int idx, void **);
+ *	Stop the RX queue at the specified index.
  */
 struct net_device_ops {
 	int			(*ndo_init)(struct net_device *dev);
@@ -1595,6 +1609,21 @@ struct net_device_ops {
 	int			(*ndo_hwtstamp_set)(struct net_device *dev,
 						    struct kernel_hwtstamp_config *kernel_config,
 						    struct netlink_ext_ack *extack);
+	void *			(*ndo_queue_mem_alloc)(struct net_device *dev,
+						       const struct netdev_cfg *dcfg,
+						       const struct netdev_rxq_cfg *qcfg,
+						       void *qmem,
+						       int idx);
+	void			(*ndo_queue_mem_free)(struct net_device *dev,
+						      const struct netdev_cfg *dcfg,
+						      const struct netdev_rxq_cfg *qcfg,
+						      void *qmem);
+	int			(*ndo_queue_start)(struct net_device *dev,
+						   int idx,
+						   void *queue_mem);
+	int			(*ndo_queue_stop)(struct net_device *dev,
+						  int idx,
+						  void **out_queue_mem);
 };
 
 /**
