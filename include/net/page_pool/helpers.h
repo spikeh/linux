8adab3193652ea535071e71782c897eecc64b55d
Pavel Begunkov <asml.silence@gmail.com>
2024-12-04 17:39:39 +0000

    net: expose page_pool_{set,clear}_pp_info
    Memory providers need to set page pool to its net_iovs on allocation, so
    expose page_pool_{set,clear}_pp_info to providers outside net/.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- include/net/page_pool/helpers.h
+++ include/net/page_pool/helpers.h
@@ -483,4 +483,17 @@ static inline void page_pool_nid_changed(struct page_pool *pool, int new_nid)
 		page_pool_update_nid(pool, new_nid);
 }
 
+#if defined(CONFIG_PAGE_POOL)
+void page_pool_set_pp_info(struct page_pool *pool, netmem_ref netmem);
+void page_pool_clear_pp_info(netmem_ref netmem);
+#else
+static inline void page_pool_set_pp_info(struct page_pool *pool,
+					 netmem_ref netmem)
+{
+}
+static inline void page_pool_clear_pp_info(netmem_ref netmem)
+{
+}
+#endif
+
 #endif /* _NET_PAGE_POOL_HELPERS_H */
