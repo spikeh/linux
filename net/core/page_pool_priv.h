0f83170b77362f6572be55c1720e6e2f15e8a776
David Wei <dw@davidwei.uk>
2024-12-12 10:53:21 -0800

    net: page pool: export page_pool_set_dma_addr_netmem()
    Export page_pool_set_dma_addr_netmem() in page_pool/helpers.h. This is
    needed by memory provider implementations that are outside of net/ to be
    able to set the dma addrs on net_iovs during alloc/free.
    
    Signed-off-by: David Wei <dw@davidwei.uk>

--- net/core/page_pool_priv.h
+++ net/core/page_pool_priv.h
@@ -13,23 +13,6 @@ int page_pool_list(struct page_pool *pool);
 void page_pool_detached(struct page_pool *pool);
 void page_pool_unlist(struct page_pool *pool);
 
-static inline bool
-page_pool_set_dma_addr_netmem(netmem_ref netmem, dma_addr_t addr)
-{
-	if (PAGE_POOL_32BIT_ARCH_WITH_64BIT_DMA) {
-		netmem_set_dma_addr(netmem, addr >> PAGE_SHIFT);
-
-		/* We assume page alignment to shave off bottom bits,
-		 * if this "compression" doesn't work we need to drop.
-		 */
-		return addr != (dma_addr_t)netmem_get_dma_addr(netmem)
-				       << PAGE_SHIFT;
-	}
-
-	netmem_set_dma_addr(netmem, addr);
-	return false;
-}
-
 static inline bool page_pool_set_dma_addr(struct page *page, dma_addr_t addr)
 {
 	return page_pool_set_dma_addr_netmem(page_to_netmem(page), addr);
