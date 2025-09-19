b61313b170647bba463b17f03cbcef36588b3dd6
David Wei <dw@davidwei.uk>
2025-09-19 23:31:34 +0200

    net: Add bind-queue operation
    Add a ynl netdev family operation called bind-queue that creates a new
    rx queue in a virtual netdev (i.e. netkit or veth) and binds it to an rx
    queue in a real netdev. This forms a queue pair, where the peer queue of
    the pair in the virtual netdev acts as a proxy for the peer queue in the
    real netdev. Thus, the peer queue in the virtual netdev can be used by
    processes running in a container to use both memory providers (io_uring
    zero-copy rx and devmem) and AF_XDP. An early implementation had only
    driver-specific integration [0], but in order for other virtual devices
    to reuse, it makes sense to have this as a generic API.
    
    src-ifindex and src-queue-id is the real netdev and its rx queue id
    respectively. dst-ifindex is the virtual netdev. Note that this op doesn't
    take dst-queue-id because a new rx queue is created. The virtual netdev
    must have real_num_rx_queues less than num_rx_queues at the time of
    calling bind-queue. The queue-type must be rx as only rx queues are
    supported for now.
    
    Signed-off-by: David Wei <dw@davidwei.uk>
    Co-developed-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://bpfconf.ebpf.io/bpfconf2025/bpfconf2025_material/lsfmmbpf_2025_netkit_borkmann.pdf [0]

--- net/core/netdev-genl-gen.h
+++ net/core/netdev-genl-gen.h
@@ -35,6 +35,7 @@ int netdev_nl_qstats_get_dumpit(struct sk_buff *skb,
 int netdev_nl_bind_rx_doit(struct sk_buff *skb, struct genl_info *info);
 int netdev_nl_napi_set_doit(struct sk_buff *skb, struct genl_info *info);
 int netdev_nl_bind_tx_doit(struct sk_buff *skb, struct genl_info *info);
+int netdev_nl_bind_queue_doit(struct sk_buff *skb, struct genl_info *info);
 
 enum {
 	NETDEV_NLGRP_MGMT,
