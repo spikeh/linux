72b72fcc265acb7053a47903e035cbab7fbca1b9
David Wei <dw@davidwei.uk>
2026-02-10 11:06:52 -0800

    [DO-NOT-INCLUDE] selftests/net: Export Netlink class via lib.py
    Making rtnl newlink calls requires constants defined in Netlink class in
    pyynl. Export it.
    
    Signed-off-by: David Wei <dw@davidwei.uk>

--- tools/testing/selftests/net/lib/py/__init__.py
+++ tools/testing/selftests/net/lib/py/__init__.py
@@ -15,7 +15,7 @@ from .nsim import NetdevSim, NetdevSimDev
 from .utils import CmdExitFailure, fd_read_timeout, cmd, bkg, defer, \
     bpftool, ip, ethtool, bpftrace, rand_port, wait_port_listen, wait_file, tool
 from .ynl import NlError, YnlFamily, EthtoolFamily, NetdevFamily, RtnlFamily, RtnlAddrFamily
-from .ynl import NetshaperFamily, DevlinkFamily, PSPFamily
+from .ynl import NetshaperFamily, DevlinkFamily, PSPFamily, Netlink
 
 __all__ = ["KSRC",
            "KsftFailEx", "KsftSkipEx", "KsftXfailEx", "ksft_pr", "ksft_eq",
@@ -30,4 +30,4 @@ __all__ = ["KSRC",
            "NetdevSim", "NetdevSimDev",
            "NetshaperFamily", "DevlinkFamily", "PSPFamily", "NlError",
            "YnlFamily", "EthtoolFamily", "NetdevFamily", "RtnlFamily",
-           "RtnlAddrFamily"]
+           "RtnlAddrFamily", "Netlink"]
